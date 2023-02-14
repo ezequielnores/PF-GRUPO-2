c54c97ab8bcf6934b98c93b07f248955463eec3d
Tebanqito <yemesesteban@gmail.com>
2023-02-14 11:37:33 -0300

    POST /historialClinico

--- api/src/routes/medicalHistoryRouter.js
+++ api/src/routes/medicalHistoryRouter.js
@@ -5,7 +5,7 @@ const {
     updateMedicalHistoryById,
     deleteMedicalHistoryById
 } = require("../controllers/medicalHistoryController");
-const { MedicalHistory, Patient } = require("../db");
+const { MedicalHistory, Patient, Doctor } = require("../db");
 const medicalHistoryRouter = Router();
 
 medicalHistoryRouter.get("/", async (req, res) => {
@@ -26,6 +26,29 @@ medicalHistoryRouter.get("/:id", async (req, res) => {
         const medicalHistory = await getMedicalHistoryById(id);
         if (!medicalHistory) throw new Error(`El historial clinico con el id ${id} no esta en la BDD.`);
         
+        res.status(200).json(medicalHistory);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+medicalHistoryRouter.post("/", async (req, res) => {
+    const { patientId, doctorId, date, diagnosis } = req.body;
+
+    try {
+        if (![patientId, doctorId, date, diagnosis].every(Boolean)) {
+            throw new Error("Datos incompletos.");
+        }
+
+        const medicalHistory = await MedicalHistory.create({
+            register: [{ doctorId, date, diagnosis }]
+        });
+
+        const patient = await Patient.findByPk(patientId);
+        if (!patient) throw new Error(`El paciente con el id ${id} no se encuentra en la BDD.`);
+
+        await medicalHistory.addPatient(patient);
+
         res.status(200).json(medicalHistory);
     } catch (error) {
         res.status(400).json({ error: error.message });
