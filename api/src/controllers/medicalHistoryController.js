378177e3630b6bfef7b003ce08971246a3a6feea
Tebanqito <yemesesteban@gmail.com>
2023-02-14 10:58:14 -0300

    medicalhistoryController

---
+++ api/src/controllers/medicalHistoryController.js
@@ -0,0 +1,21 @@
+const { MedicalHistory, Patient } = require("../db");
+
+const getMedicalHistoryById = async id => {
+    const medicalHistory = await MedicalHistory.findByPk(id, { include: [Patient] });
+    return medicalHistory;
+};
+
+const findAllMedicalHistory = async () => {
+    const medicalHistories = await MedicalHistory.findAll();
+    return medicalHistories;
+};
+
+const deleteMedicalHistoryById = async id => {
+    const medicalHistoryDeleted = await MedicalHistory.destroy({ where: { id: id } });
+    return medicalHistoryDeleted;
+};
+
+const updateMedicalHistoryById = async (attributes, id) => {
+    const medicalHistoryUpdated = await MedicalHistory.update(attributes, { where: { id: id } });
+    return medicalHistoryUpdated;
+};
