e8c24ce17efe4ce787f57b126749a0c1228247be
Tebanqito <yemesesteban@gmail.com>
2023-02-13 21:01:46 -0300

    turnos  modelado y enrutado con post y sus gets

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,13 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const turnsRouter = require("../routes/turnsRouter");
 
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.use("/turns", turnsRouter);
 
 module.exports = router;
