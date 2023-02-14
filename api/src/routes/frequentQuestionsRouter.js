5bc9aec98e2793d5eaa160c42c0b85a6dcb6811b
Tebanqito <yemesesteban@gmail.com>
2023-02-14 15:07:59 -0300

    GET /preguntaFrecuente:id

--- api/src/routes/frequentQuestionsRouter.js
+++ api/src/routes/frequentQuestionsRouter.js
@@ -19,4 +19,18 @@ frequentQuestionsRouter.get("/", async (req, res) => {
     }
 });
 
+frequentQuestionsRouter.get("/:id", async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        if (!id) throw new Error("El id de la pregunta frecuente esta indefinido.");
+        const frequentAsk = await getFrequentAskById(id);
+        if (!frequentAsk) throw new Error(`La pregunta frecuente con el id ${id} no esta en la BDD.`);
+
+        res.status(200).json(frequentAsk);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
 module.exports = frequentQuestionsRouter;
