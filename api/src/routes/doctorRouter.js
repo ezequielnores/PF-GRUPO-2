8679f367ef52f53a75391bf5de3e171070e0fedc
Tomas Guignet <tomasguignet9@gmail.com>
2023-02-15 11:36:52 -0300

    Ruta Doctor / POST y PUT / Controlador

--- api/src/routes/doctorRouter.js
+++ api/src/routes/doctorRouter.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { getDoctors, getDoctor } = require("../controllers/doctorController");
+const { getDoctors, getDoctor, postDoctor, putDoctor } = require("../controllers/doctorController");
 
 const router = Router()
 
@@ -26,3 +26,27 @@ router.get("/:id", (req, res) => {
         res.status(404).send("This doctor is not in Data Base")
     }
 })
+
+router.post("/", async (req, res) => {
+    const {name, lastName, mail, password, birthdate, image, location, dni, phone, speciality, lisence, cv, clinicMail} = req.body;
+    try {
+        const doctor = await postDoctor(name, lastName, mail, password, birthdate, image, location, dni, phone, speciality, lisence, cv, clinicMail);
+        if (!doctor) throw new Error();
+        res.status(200).send(doctor);
+    } catch (error) {
+        res.status(400).send("The doctor was not created");
+    }
+})
+
+router.put("/update/:id", async (req, res) => {
+    const {id} = req.params;
+    const values = req.body;
+    try {
+        const doctor = await putDoctor(id, values);
+        res.status(200).send(doctor);
+    } catch (error) {
+        res.status(400).send("The doctor was not updated")
+    }
+})
+
+module.exports = router;
