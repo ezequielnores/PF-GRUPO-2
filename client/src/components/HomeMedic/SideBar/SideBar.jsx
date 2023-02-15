099685d2e9284f91d1aa83447f9458e21dde1d31
Laoset <xlaospt2@gmail.com>
2023-02-15 13:13:03 -0300

    Elimine rutas app, modifique HomeMedic y sus rutas, agregue Profile medic

--- client/src/components/HomeMedic/SideBar/SideBar.jsx
+++ client/src/components/HomeMedic/SideBar/SideBar.jsx
@@ -1,27 +1,85 @@
 import { Link } from "react-router-dom";
-
-const SideBar =()=>{
-
-return(
-
+import style from "./SideBar.module.css";
+import Stack from "@mui/material/Stack";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import MedicalServicesIcon from "@mui/icons-material/MedicalServices";
+import RateReviewIcon from "@mui/icons-material/RateReview";
+import LogoutIcon from "@mui/icons-material/Logout";
+const SideBar = ({ open, handleOpen }) => {
+  return (
     <div>
-         <button>
-            <Link to="/">Back to LandingPage</Link>
-        </button>
-         <button>
-            <Link to="/HomeMedic/Profile">Profile</Link>
-        </button>
-         <button>
-            <Link to="/HomeMedic/Agenda">Agenda</Link>
-        </button>
-         <button>
-            <Link to="/HomeMedic/MedicalEmergency">Medical Emergency</Link>
-        </button>
-         <button>
-            <Link to="/HomeMedic/Reviews">Reviews</Link>
-        </button>
+      <div style={{ position: "fixed", left: "0", top: "9rem" }}>
+        <Stack spacing={6}>
+          <Stack spacing={1} width={open ? 200 : 45}>
+            <button onClick={handleOpen} style={{ textAlign: "end" }}>
+              <MenuIcon />
+            </button>
+            <button>
+              <Link to="/HomeMedic/Profile">
+                {open ? (
+                  <div className={style.divbutton}>
+                    <AccountCircleIcon />
+                    Profile
+                  </div>
+                ) : (
+                  <AccountCircleIcon />
+                )}
+              </Link>
+            </button>
+            <button>
+              <Link to="/HomeMedic/Agenda">
+                {open ? (
+                  <div className={style.divbutton}>
+                    <CalendarMonthIcon />
+                    My Appointments
+                  </div>
+                ) : (
+                  <CalendarMonthIcon />
+                )}
+              </Link>
+            </button>
+            <button>
+              <Link to="/HomeMedic/MedicalEmergency">
+                {open ? (
+                  <div className={style.divbutton}>
+                    <MedicalServicesIcon />
+                    Urgency
+                  </div>
+                ) : (
+                  <MedicalServicesIcon />
+                )}
+              </Link>
+            </button>
+            <button>
+              <Link to="/HomeMedic/Reviews">
+                {open ? (
+                  <div className={style.divbutton}>
+                    <RateReviewIcon />
+                    Reviews
+                  </div>
+                ) : (
+                  <RateReviewIcon />
+                )}
+              </Link>
+            </button>
+          </Stack>
+          <button style={{ background: "#307196" }}>
+            <Link to="/">
+              {open ? (
+                <div className={style.divbutton} style={{ color: "white" }}>
+                  <LogoutIcon />
+                  Logout
+                </div>
+              ) : (
+                <LogoutIcon style={{ color: "white" }} />
+              )}
+            </Link>
+          </button>
+        </Stack>
+      </div>
     </div>
-    )
-}
-
+  );
+};
 export default SideBar;
