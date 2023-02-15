9d7630759f21d53ca5fd9545465763fd77d5e56c
Tebanqito <yemesesteban@gmail.com>
2023-02-15 09:59:06 -0300

    GET /ingresos:id

--- api/src/routes/incomesRouter.js
+++ api/src/routes/incomesRouter.js
@@ -19,4 +19,20 @@ incomesRouter.get("/", async (req, res) => {
     }
 });
 
+incomesRouter.get("/:id", async (req, res) => {
+    const { id } = req.params;
+
+
+    try {
+        if (!id) throw new Error("Error al obtener el id desde params.");
+
+        const income = await getIncomeById(id);
+        if (!income) throw new Error(`No se encuentra ningun ingreso en la BDD con el id ${id}.`);
+
+        res.status(200).json(income);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
 module.exports = incomesRouter;
