714fb9801bd8a0e0415c07f8365f7f44152c231e
Tebanqito <yemesesteban@gmail.com>
2023-02-15 21:22:24 -0300

    findAllMedicalHistoryByPatient exportado

--- api/src/controllers/medicalHistoryController.js
+++ api/src/controllers/medicalHistoryController.js
@@ -58,5 +58,6 @@ module.exports = {
     findAllMedicalHistory,
     deleteMedicalHistoryById,
     updateMedicalHistoryById,
-    addRegisterMedicalHistory
+    addRegisterMedicalHistory,
+    findAllMedicalHistoryByPatient
 };
