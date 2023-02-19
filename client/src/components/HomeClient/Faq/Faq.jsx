7c1663d5560293054e0976ae0085f4ca0c62493e
AntoAldao <antoaldao@gmail.com>
2023-02-19 17:38:04 -0300

    conexion del home, mis turnos, urgencias y preguntas frecuentes con el back, ademas cambio en getFrequentAskbyAsk de la ruta de preguntas frecuentes

--- client/src/components/HomeClient/Faq/Faq.jsx
+++ client/src/components/HomeClient/Faq/Faq.jsx
@@ -8,37 +8,27 @@ import Paper from '@mui/material/Paper';
 import TablePagination from '@mui/material/TablePagination';
 import RowsFaq from './RowsFaq';
 import style from './Faq.module.css';
-import Button from '@mui/material/Button';
+import axios from 'axios';
+import { useEffect } from 'react';
 const Faq = () => {
-    const faqRedux = [
-        {
-            id: 1,
-            question: "¿Qué es la telemedicina?",
-            answer: "La telemedicina es la utilización de las tecnologías de la información y la comunicación para la prestación de servicios de salud a distancia. La telemedicina permite que los profesionales de la salud brinden atención médica a distancia, utilizando medios de comunicación como teléfonos, computadoras, televisión y otros dispositivos electrónicos. La telemedicina permite que los profesionales de la salud brinden atención médica a distancia, utilizando medios de comunicación como teléfonos, computadoras, televisión y otros dispositivos electrónicos."
-        },
-        {
-            id: 2,
-            question: "¿Qué es una operacion?",
-            answer:"una operacion es una intervencion quirurgica, que se realiza para corregir una enfermedad o una deformidad, o para extirpar un tumor. La operacion es una intervencion quirurgica, que se realiza para corregir una enfermedad o una deformidad, o para extirpar un tumor."
-        },
-        {
-            id: 3,
-            question:"¿Como puedo pedir turno?",
-            answer:"Para pedir turno, debes ingresar a la pagina web, y registrarte como usuario, dirigirte a la seccion de turnos, y seleccionar el medico que quieras, y la fecha y hora que quieras."
-        },
-        {
-            id: 4,
-            question:"Necesito un medico urgente, ¿como puedo conseguirlo?",
-            answer:"Para conseguir un medico urgente, debes ingresar a la pagina web, y registrarte como usuario, dirigirte a la seccion de urgencias, completar el formulario, y esperar a que un medico se ponga en contacto con vos."
-        },
-        ]
+    const [faq, setFaq] = React.useState([]);
+    const [allFaq, setAllFaq] = React.useState([]);
+    useEffect(() => {
+        axios.get('http://localHost:3001/frequentQuestions')
+        .then(res => {
+            setAllFaq(res.data)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+    useEffect(() => {
+        setFaq(allFaq)
+    }, [allFaq])
 
 
+     
     const [page, setPage] = React.useState(0);
     const [search, setSearch] = React.useState("");
-
-    const faq = faqRedux;
-
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -48,11 +38,22 @@ const Faq = () => {
         setSearch(e.target.value);
     };
 
-    const handleSearch = () => {
-        console.log(search)
-    }
-
-
+    const handleSearch = (event) => {
+        event.preventDefault();
+        const body = {
+            ask: search
+        }
+        axios.get(`${process.env.REACT_APP_BACKEND_URL}/frequentQuestions/frequentAskByAsk`,{params:body})
+        .then(res => {
+            setFaq(res.data)
+        })
+        .catch(err => console.log(err))
+    };
+    useEffect(() => {
+        if(search === ""){
+            setFaq(allFaq)
+        }
+    }, [search])
     return (
         <div style={{display:"flex",flexDirection:"column",alignItems:"center",justifyContent:"space-between",padding:"0 2vw 0 5vw",width:"90vw",gap:"2vh"}}>
 
@@ -79,7 +80,7 @@ const Faq = () => {
                                 {faq[0]?
                                     
                                 faq.slice(page*4,(page*4)+4).map((f) => (
-                                <RowsFaq key={f.id} question={f.question} answer={f.answer}/>
+                                <RowsFaq key={f.id} question={f.ask} answer={f.answer}/>
                                 ))
                                 :
                                 <TableRow>
@@ -95,7 +96,7 @@ const Faq = () => {
                                     rowsPerPage= {4}
                                     page={page}
                                     labelRowsPerPage = {""}
-                                    rowsPerPageOptions = {""}
+                                    rowsPerPageOptions = {[]}
                                     onPageChange={handleChangePage}
                             />
                 </TableContainer>
