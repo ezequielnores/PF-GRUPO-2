24fe46798567fe41ccd767b3bc967d21dd7f10fd
Tebanqito <yemesesteban@gmail.com>
2023-03-02 00:46:11 -0300

    ButtonsFQ con estilos comentados que hacen no funcionar bien el flujo

--- client/src/components/DashboardAdmin/ButtonsFQ.jsx
+++ client/src/components/DashboardAdmin/ButtonsFQ.jsx
@@ -1,5 +1,28 @@
 import React from "react";
-import styled from "styled-components";
+// import styled from "styled-components";
+
+// const ButtonsContainer = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   margin-bottom: 20px;
+// `;
+
+// const Button = styled.button`
+//   background-color: #f8f8f8;
+//   border: none;
+//   border-radius: 3px;
+//   color: #222;
+//   cursor: pointer;
+//   font-size: 1rem;
+//   margin-right: 10px;
+//   padding: 8px 16px;
+//   transition: all 0.2s ease-in-out;
+
+//   &:hover {
+//     background-color: #222;
+//     color: #f8f8f8;
+//   }
+// `;
 
 const ButtonsFQ = ({ createFQ, readFQ, read, create}) => {
     return (
