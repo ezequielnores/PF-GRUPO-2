15d854c857fce897a255f69f96e50f88771cfd51
Nicolas <nicobouvet7@gmail.com>
2023-03-01 22:11:53 -0300

    Agrego nueva seccion review para no pacientes y otras modificaciones

--- client/src/components/ChatBot/LinkReview.jsx
+++ client/src/components/ChatBot/LinkReview.jsx
@@ -4,7 +4,7 @@ const LinkReview =()=> {
         return (
             <div>
                 <p>If you want to see all the reviews about our medical specialists, click the following link and you will be redirected to the reviews section.</p>
-                <a  href="http://localhost:3000/totalReviews" target="_blank" style={{color:"black",textDecoration:"underline", fontWeight:"bold"}}>Reviews</a>
+                <a  href="https://pf-grupo-2.vercel.app/totalReviews" target="_blank" style={{color:"black",textDecoration:"underline", fontWeight:"bold"}}>Reviews</a>
             </div>
         )
 }
